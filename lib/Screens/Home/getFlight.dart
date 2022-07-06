fe15965512652b0732d7d70606ea886d9a2e0206
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-07-06 18:57:19 +0100

    ui work

--- lib/Screens/Home/getFlight.dart
+++ lib/Screens/Home/getFlight.dart
@@ -37,22 +37,107 @@ class _GetFlightState extends State<GetFlight> {
     }
   }
 
-  //"Airline Name: ${flight![0].airline!.name} \n Departure: ${flight![0].departure!.iata}\n Departure Time: ${flight![0].departure!.scheduled}(${flight![0].departure!.timezone})\n Departure Terminal:${flight![0].departure!.terminal} \n Departure Gate:${flight![0].departure!.gate} \n Arrival: ${flight![0].arrival!.iata} \n Arrival Terminal:${flight![0].arrival!.terminal}\nArrival Time ${flight![0].arrival!.scheduled}(${flight![0].arrival!.timezone})\n Arrival Gate:${flight![0].arrival!.gate} \n Flight Number: ${flight![0].flight!.number} \n Flight Status: ${flight![0].flightStatus}"),
+//"Airline Name: ${flight![i].airline!.name}\nNumber: ${flight![i].flight!.number}\nArrival: ${flight![i].arrival!.iata}\nArrival Time: ${flight![i].arrival!.scheduled}(${flight![i].arrival!.timezone})\nArrival Terminal:${flight![i].arrival!.terminal}\nArrival Gate:${flight![i].arrival!.gate}\nDeparture: ${flight![i].departure!.iata}\nDeparture Time: ${flight![i].departure!.scheduled}(${flight![i].departure!.timezone})\nDeparture Terminal:${flight![i].departure!.terminal}\nDeparture Gate:${flight![i].departure!.gate}\nFlight Status: ${flight![i].flightStatus}",
 
   whatToDo() {
     if (isFound && isLoaded) {
+      print(flight![0].departure!.scheduled);
       return SafeArea(
           child: Scaffold(
+        backgroundColor: Colors.transparent,
         body: ListView.builder(
             itemCount: flight!.length,
             itemBuilder: (context, i) {
               return ListTile(
-                  title: Text(
-                    "Airline Name: ${flight![i].airline!.name}\nNumber: ${flight![i].flight!.number}\nArrival: ${flight![i].arrival!.iata}\nArrival Time: ${flight![i].arrival!.scheduled}(${flight![i].arrival!.timezone})\nArrival Terminal:${flight![i].arrival!.terminal}\nArrival Gate:${flight![i].arrival!.gate}\nDeparture: ${flight![i].departure!.iata}\nDeparture Time: ${flight![i].departure!.scheduled}(${flight![i].departure!.timezone})\nDeparture Terminal:${flight![i].departure!.terminal}\nDeparture Gate:${flight![i].departure!.gate}\nFlight Status: ${flight![i].flightStatus}",
-                    style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w500, color: Colors.black),
-                  ),
-                  subtitle: const Divider(color: Colors.black));
+                title: Container(
+                    height: SizeConfig.screenWidth / 2.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.all(15),
+                    // margin: EdgeInsets.all(20),
+                    child: Stack(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Flexible(
+                                    flex: 1,
+                                    fit: FlexFit.loose,
+                                    child: Container(
+                                        color: Colors.transparent,
+                                        width: SizeConfig.screenWidth / 1.2,
+                                        height: SizeConfig.screenWidth / 4.5,
+                                        child: Column(
+                                          children: <Widget>[
+                                            Text(
+                                                "${flight![i].departure!.iata}",
+                                                style: GoogleFonts.stylish(
+                                                  fontSize:
+                                                      SizeConfig.screenWidth /
+                                                          20,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Colors.grey,
+                                                )),
+                                            Text(
+                                                " ${flight![i].departure!.scheduled.toString().substring(11, 16)}"),
+                                          ],
+                                        )),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    flex: 3,
+                                    child: Container(
+                                      color: Colors.grey,
+                                      width: SizeConfig.screenWidth / 10,
+                                      height: SizeConfig.screenWidth / 4.5,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Flexible(
+                                    flex: 1,
+                                    fit: FlexFit.loose,
+                                    child: Container(
+                                        color: Colors.transparent,
+                                        width: SizeConfig.screenWidth / 1.2,
+                                        height: SizeConfig.screenWidth / 4.5,
+                                        child: Column(
+                                          children: <Widget>[
+                                            Text(
+                                                "${flight![i].departure!.iata}",
+                                                style: GoogleFonts.stylish(
+                                                  fontSize:
+                                                      SizeConfig.screenWidth /
+                                                          20,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Colors.grey,
+                                                )),
+                                            Text(
+                                                " ${flight![i].departure!.scheduled.toString().substring(11, 16)}"),
+                                          ],
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )),
+
+                //   Text(
+                //       "Arrival: ${flight![i].arrival!.iata}\nArrival Time: ${flight![i].arrival!.scheduled}(${flight![i].arrival!.timezone})\nDeparture: ${flight![i].departure!.iata}\nDeparture Time: ${flight![i].departure!.scheduled}(${flight![i].departure!.timezone})\nFlight Status: ${flight![i].flightStatus}",
+                //       style: TextStyle(fontSize: 25)),
+                // ),
+              );
             }),
       ));
     }
@@ -63,7 +148,7 @@ class _GetFlightState extends State<GetFlight> {
     SizeConfig.init(context);
 
     return CupertinoPageScaffold(
-        backgroundColor: Color.fromARGB(255, 249, 240, 240),
+        backgroundColor: Color.fromARGB(255, 230, 241, 241),
         child: SingleChildScrollView(
             child: Stack(
           children: <Widget>[
